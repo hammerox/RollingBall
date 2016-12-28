4b45f67fdecd5216046f9a3b5f5f01e47465b996
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-12-28 18:26:09 -0200

    Android screen light always on

--- android/src/com/hammerox/rollingbal/AndroidLauncher.java
+++ android/src/com/hammerox/rollingbal/AndroidLauncher.java
@@ -1,10 +1,10 @@
 package com.hammerox.rollingbal;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.hammerox.rollingbal.RollingBallGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
@@ -12,5 +12,8 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new RollingBallGame(), config);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 	}
 }
