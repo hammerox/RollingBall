ffe15f70acef9f10b79fd46506e57beb65fe5bfe
Mauricio Custodio <mauricio_rc2@hotmail.com>
2017-01-04 12:08:58 -0200

    Added Spikes to Android

--- android/src/com/hammerox/rollingbal/LevelActivity.java
+++ android/src/com/hammerox/rollingbal/LevelActivity.java
@@ -8,37 +8,47 @@ import android.view.View;
 
 import com.hammerox.rollingbal.databinding.ActivityLevelBinding;
 
-public class LevelActivity extends AppCompatActivity {
+import static com.hammerox.rollingbal.RollingBallGame.*;
+
+public class LevelActivity extends AppCompatActivity implements View.OnClickListener{
 
     public final static String TAG_LEVEL = "level";
 
-    View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(LevelActivity.this, AndroidLauncher.class);
-
-            switch (view.getId()) {
-                case R.id.button_casual:
-                    intent.putExtra(TAG_LEVEL, RollingBallGame.Level.CASUAL.name());
-                    break;
-                case R.id.button_speed:
-                    intent.putExtra(TAG_LEVEL, RollingBallGame.Level.SPEED.name());
-                    break;
-                default:
-                    intent.putExtra(TAG_LEVEL, RollingBallGame.Level.SPEED.name());
-                    break;
-            }
-
-            startActivity(intent);
-        }
-    };
+    /* To add a new level:
+    *   onClick  - Add level name to button
+    *   onCreate - Bind clickListener to button
+    * */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityLevelBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_level);
 
-        binding.buttonCasual.setOnClickListener(clickListener);
-        binding.buttonSpeed.setOnClickListener(clickListener);
+        binding.buttonCasual.setOnClickListener(this);
+        binding.buttonSpeed.setOnClickListener(this);
+        binding.buttonSpikes.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(LevelActivity.this, AndroidLauncher.class);
+        String levelName;
+
+        switch (view.getId()) {
+            case R.id.button_casual:
+                levelName = Level.CASUAL.name();
+                break;
+            case R.id.button_speed:
+                levelName = Level.SPEED.name();
+                break;
+            case R.id.button_spikes:
+                levelName = Level.SPIKES.name();
+                break;
+            default:
+                levelName = Level.SPEED.name();
+        }
+
+        intent.putExtra(TAG_LEVEL, levelName);
+        startActivity(intent);
     }
 }
