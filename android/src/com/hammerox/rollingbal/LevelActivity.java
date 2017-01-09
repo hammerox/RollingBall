389cb8e91bc8193d5375848d0cb0198ea2c26e49
Mauricio Custodio <mauricio_rc2@hotmail.com>
2017-01-09 20:32:50 -0200

    Refactored: Buttons now are created automatically.

--- android/src/com/hammerox/rollingbal/LevelActivity.java
+++ android/src/com/hammerox/rollingbal/LevelActivity.java
@@ -1,54 +1,60 @@
 package com.hammerox.rollingbal;
 
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.hammerox.rollingbal.databinding.ActivityLevelBinding;
 
 import static com.hammerox.rollingbal.RollingBallGame.*;
 
-public class LevelActivity extends AppCompatActivity implements View.OnClickListener{
+public class LevelActivity extends AppCompatActivity {
 
     public final static String TAG_LEVEL = "level";
 
-    /* To add a new level:
-    *   onClick  - Add level name to button
-    *   onCreate - Bind clickListener to button
-    * */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityLevelBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_level);
 
-        binding.buttonCasual.setOnClickListener(this);
-        binding.buttonSpeed.setOnClickListener(this);
-        binding.buttonSpikes.setOnClickListener(this);
+        ButtonFactory.createButtons(this, binding.activityLevel);
     }
 
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(LevelActivity.this, AndroidLauncher.class);
-        String levelName;
+}
 
-        switch (view.getId()) {
-            case R.id.button_casual:
-                levelName = Level.CASUAL.name();
-                break;
-            case R.id.button_speed:
-                levelName = Level.SPEED.name();
-                break;
-            case R.id.button_spikes:
-                levelName = Level.SPIKES.name();
-                break;
-            default:
-                levelName = Level.SPEED.name();
+
+class ButtonFactory {
+
+    private static LinearLayout.LayoutParams params =
+            new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+
+    private static View.OnClickListener clickListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(view.getContext(), AndroidLauncher.class);
+                    String levelName = ((Button) view).getText().toString();
+                    intent.putExtra(LevelActivity.TAG_LEVEL, levelName);
+                    view.getContext().startActivity(intent);
+                }
+            };
+
+    static void createButtons(Context context, LinearLayout layout) {
+        for (Level level: Level.class.getEnumConstants()) {
+            Button button = new Button(context);
+            button.setLayoutParams(params);
+            button.setText(level.name());
+            button.setOnClickListener(clickListener);
+            layout.addView(button);
         }
-
-        intent.putExtra(TAG_LEVEL, levelName);
-        startActivity(intent);
     }
 }
